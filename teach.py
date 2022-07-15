73e2b782e6d61991a7bd736005822300756170f3
Aditya-jain23 <kumaradityaj.aj@gmail.com>
2022-07-15 20:08:07 +0530

    Updated

---
+++ teach.py
@@ -0,0 +1,82 @@
+""" 
+THIS PROGRAM IS SPECIALLY CREATED TO TEACH STUDENTS THE 
+IMPORTANCE OF HOW A VARIABLE IS STORED IN THE MEMORY 
+"""
+##########################################################################################################
+# VARIABLES
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+##########################################################################################################
+# DATA TYPES 
+
+# Text Type: str
+# Numeric Types: int, float, complex
+# Sequence Types: list, tuple, range
+# Mapping Type:	dict
+# Boolean Type:	bool
+
+##########################################################################################################
+
+# a=5
+# b=6
+# print(a+b)
+
+# example_list = [1,2,3,4,5]
+# print(example_list[0]+example_list[1])
+
+# a='5'
+# b='6'
+# print(a+b)
+
+# k=input("Enter a number: ")
+# j=input("Enter another number: ")
+# print(k+j)
+
+
+# list2 =[]
+# l = input("Please enter the length of the list")
+# #for i in range(l):
+# for i in range(int(l)):
+#     list2.append(input("Input Number: "))
+# print(list2)    
+
+
+#ERROR CODE-1
+# def example(list_as_parameter):
+#     for i in list_as_parameter:
+#         print(i+1)
+
+# list3=[]
+# length = int(input("Enter length of the array= "))
+# for i in range(length):
+#     list3.append(input("Input Number: "))  
+
+# example(list3)
+
+#ERROR CODE-2
+# def example(list_as_parameter):
+#     for i in list_as_parameter:
+#         print(i+i)
+
+# list3=[]
+# length = int(input("Enter length of the array= "))
+# for i in range(length):
+#     list3.append(input("Input Number: "))  
+
+# example(list3)
+
+#RIGHT CODE
+# def example(list_as_parameter):
+#     for i in list_as_parameter:
+#         print(i+1)
+
+# list3=[]
+# length = int(input("Enter length of the array= "))
+# for i in range(length):
+#     list3.append(int(input("Input Number: ")))  
+
+# example(list3)
+
